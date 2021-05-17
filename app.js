3249c68446a26af7e54d5b264b9df0cd8023ad78
aswaysway <kevarez63@gmail.com>
2021-05-16 23:54:57 -0500

    update

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container');
+const hamburger = document.querySelector(".header .nav-bar .nav-list .hamburger");
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const menu_item = document.querySelectorAll(".header .nav-bar .nav-list ul li a");
+const header = document.querySelector(".header.container");
+const headerColor = "";
 
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
 });
 
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
+document.addEventListener("scroll", () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 150) {
+    header.style.backgroundColor = "#141e30";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
 });
 
 menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
-	});
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
 });
 
-var hero_img = document.querySelectorAll("#hero-img img"), i = 1;
-Array.prototype.forEach.call(hero_img, function(img) { 
-    setTimeout(function(){ img.classList.add("visible") }, 700*i)
-i++;
-})
+var hero_img = document.querySelectorAll("#hero-img img"),
+  i = 1;
+Array.prototype.forEach.call(hero_img, function (img) {
+  setTimeout(function () {
+    img.classList.add("visible");
+  }, 700 * i);
+  i++;
+});
